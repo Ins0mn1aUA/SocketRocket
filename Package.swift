4debbadd2b9c519954218c8638f088be99864f61
Grisha Stetsenko <42353983+Ins0mn1aUA@users.noreply.github.com>
2025-07-07 17:51:50 +0300

    ++

--- Package.swift
+++ Package.swift
@@ -1,21 +1,18 @@
-// swift-tools-version:5.3
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "SocketRocket",
-    platforms: [
-        .iOS(.v13)
-    ],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "SocketRocket", targets: ["SocketRocket"]),
     ],
-    dependencies: [],
     targets: [
-        .target(
-            name: "SocketRocket",
-            path: "SocketRocket",
-            cSettings: [
-                .headerSearchPath("Internal/**"),
-            ]),
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "SocketRocket"),
+        .testTarget(name: "SocketRocketTests", dependencies: ["SocketRocket"]),
     ]
 )
